e40a3b3512fd4b0e82a1b676a9e7851d151cd6ab
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-29 08:42:34 +0300

    Create minecraft.sh

---
+++ minecraft.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl -Lo /usr/bin/minecraft-launcher https://github.com/GabiNun/Script/raw/main/minecraft-launcher
+curl -Lo /usr/bin/minecraft-launcher.png https://github.com/GabiNun/Script/raw/main/minecraft-launcher.png
+chmod +x /usr/bin/minecraft-launcher
+cat > /usr/share/applications/minecraft-launcher.desktop <<EOF
+[Desktop Entry]
+Name=Minecraft Launcher
+Comment=Custom Minecraft launcher
+Exec=env GTK_IM_MODULE=none QT_IM_MODULE=none XMODIFIERS= __GLFW_IM_MODULE=none /usr/bin/minecraft-launcher
+Icon=/usr/bin/minecraft-launcher.png
+Terminal=false
+Type=Application
+Categories=Game;
+EOF
