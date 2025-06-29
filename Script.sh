66c8fa15b42f5e1bdc860475d64046b3921e76cb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-29 08:58:29 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -17,7 +17,8 @@ apt-get -y install steam virt-manager gnome-shell-extension-manager default-jdk 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install -y flathub org.vinegarhq.Sober
 
 snap remove firefox firmware-updater desktop-security-center
-curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/brave.sh | sh
+curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Brave.sh | sh
+curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Minecraft.sh | sudo sh
 
 usermod -aG libvirt "$(logname)"
 username=$(whoami); echo "$username ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/99-nopasswd && chmod 440 /etc/sudoers.d/99-nopasswd
