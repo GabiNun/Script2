d6177b5a52e414f58ddc7f63dfd1d00bb951eaea
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-29 08:57:10 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -19,17 +19,5 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 snap remove firefox firmware-updater desktop-security-center
 curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/brave.sh | sh
 
-curl -o Minecraft.deb https://launcher.mojang.com/download/Minecraft.deb && apt-get -y install ./Minecraft.deb && rm Minecraft.deb && mkdir -p ~/.local/share/applications && cat > ~/.local/share/applications/minecraft-launcher.desktop <<EOF
-[Desktop Entry]
-Name=Minecraft Launcher
-Comment=Launch Minecraft
-Exec=env GTK_IM_MODULE=none QT_IM_MODULE=none XMODIFIERS= __GLFW_IM_MODULE=none minecraft-launcher
-Icon=minecraft-launcher
-Terminal=false
-Type=Application
-Categories=Game;Application;
-StartupWMClass=minecraft-launcher
-EOF
-
 usermod -aG libvirt "$(logname)"
 username=$(whoami); echo "$username ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/99-nopasswd && chmod 440 /etc/sudoers.d/99-nopasswd
