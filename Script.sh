22c62a85ac58f1998055be8a235e6a2b48c532a6
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-06-29 10:08:25 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -13,12 +13,11 @@ add_nodisplay /usr/share/applications/gnome-session-properties.desktop
 sed -i '/^NoDisplay=true/d; /^Type=Application/a NoDisplay=true' /usr/share/applications/org.gnome.TextEditor.desktop
 
 dpkg --add-architecture i386 && apt-get update -y
-apt-get -y install steam virt-manager gnome-shell-extension-manager plasma-discover flatpak && apt-get remove -y firefox
+apt-get -y install steam gnome-shell-extension-manager plasma-discover flatpak && apt-get remove -y firefox
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install -y flathub org.vinegarhq.Sober
 
 snap remove firefox firmware-updater desktop-security-center
 curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Brave.sh | sh
 curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Minecraft.sh | sh
 
-usermod -aG libvirt "$(logname)"
 username=$(whoami); echo "$username ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/99-nopasswd && chmod 440 /etc/sudoers.d/99-nopasswd
