a1bbb992fc300d4650dfd51b0287ee9de079682f
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-01 02:20:03 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -15,4 +15,5 @@ snap remove firefox firmware-updater desktop-security-center
 curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Brave.sh | sh
 curl -fsSL https://raw.githubusercontent.com/GabiNun/Script/main/Minecraft.sh | sh
 
-username=$(whoami); echo "$username ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/99-nopasswd && chmod 440 /etc/sudoers.d/99-nopasswd
+echo "$(whoami) ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/99-nopasswd
+sudo chmod 440 /etc/sudoers.d/99-nopasswd
