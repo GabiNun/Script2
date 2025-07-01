b9805fd7e29429e20749817b6e12644196b0c107
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-01 12:03:30 +0300

    Update Minecraft.sh

--- Minecraft.sh
+++ Minecraft.sh
@@ -1,6 +1,6 @@
 curl -sLo /usr/bin/minecraft-launcher https://github.com/GabiNun/Script/raw/main/minecraft-launcher && curl -sLo /usr/bin/minecraft-launcher.png https://github.com/GabiNun/Script/raw/main/minecraft-launcher.png
 
-
+curl -sL https://launcher.mojang.com/download/Minecraft.tar.gz | tar -xz -C .
 curl -sL "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/12/Minecraft_Launcher_MS_Icon.png/revision/latest?cb=20230618110121" -o /usr/bin/minecraft-launcher.png
 chmod +x /usr/bin/minecraft-launcher
 cat > /usr/share/applications/minecraft-launcher.desktop <<EOF
