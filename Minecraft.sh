5009c2b0c9ed87a0d5cc778204a16e64835ec4eb
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-07-01 12:07:58 +0300

    Update Minecraft.sh

--- Minecraft.sh
+++ Minecraft.sh
@@ -1,16 +1,14 @@
 curl -sLo /usr/bin/minecraft-launcher https://github.com/GabiNun/Script/raw/main/minecraft-launcher && curl -sLo /usr/bin/minecraft-launcher.png https://github.com/GabiNun/Script/raw/main/minecraft-launcher.png
 
-curl -sL https://launcher.mojang.com/download/Minecraft.tar.gz | tar -xz -C .
-sudo mv /home/$(logname)/minecraft-launcher/minecraft-launcher /usr/bin/
-rm -rf /home/$(logname)/minecraft-launcher
-curl -sL "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/12/Minecraft_Launcher_MS_Icon.png/revision/latest?cb=20230618110121" -o /usr/bin/minecraft-launcher.png
+sudo curl -sL https://launcher.mojang.com/download/Minecraft.tar.gz | sudo tar -xz -C /usr/bin/
+curl -sL "https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/12/Minecraft_Launcher_MS_Icon.png/revision/latest?cb=20230618110121" -o /usr/bin/minecraft-launcher/minecraft-launcher.png
 chmod +x /usr/bin/minecraft-launcher
 cat > /usr/share/applications/minecraft-launcher.desktop <<EOF
 [Desktop Entry]
 Name=Minecraft Launcher
 Comment=Custom Minecraft launcher
-Exec=env GTK_IM_MODULE=none QT_IM_MODULE=none XMODIFIERS= __GLFW_IM_MODULE=none /usr/bin/minecraft-launcher
-Icon=/usr/bin/minecraft-launcher.png
+Exec=env GTK_IM_MODULE=none QT_IM_MODULE=none XMODIFIERS= __GLFW_IM_MODULE=none /usr/bin/minecraft-launcher/minecraft-launcher
+Icon=/usr/bin/minecraft-launcher/minecraft-launcher.png
 Terminal=false
 Type=Application
 Categories=Game;
